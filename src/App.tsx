771f3a638da7228a26e5aaf51589088aaf2beead
Sergey Sokurenko <s.sokurenko@gmail.com>
2019-08-10 18:10:11 +0200

    Added Colors component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import { Colors } from './components'
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Colors</h1>
+      <Colors />
     </div>
   );
 }
---
+++ src/components/Colors.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Colors: React.FC = () => {
+  return <h1>List</h1>
+}
+
+export default Colors;
